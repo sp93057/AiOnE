f1a9e084b3bb3d9dabe343b00fb84ed7a92e22f3
swarajkm <102534144+swarajkm@users.noreply.github.com>
2023-04-14 18:54:05 +0530

    Improved the feedback form page

--- AiOnE_v1/src/styles/Homescreen.styles.js
+++ AiOnE_v1/src/styles/Homescreen.styles.js
@@ -1,117 +1,131 @@
 import { StyleSheet } from "react-native";
 export default StyleSheet.create({
-    topContainer: {
-      height: '20%',
-      width: '100%',
-      paddingHorizontal: 35,
-      paddingTop: 80,
-      paddingBottom: 40,
-      backgroundColor: '#432344',
-      borderBottomLeftRadius: 24,
-      borderBottomRightRadius: 24,
-      overflow: 'hidden',
-      flexDirection: 'row',
+
+  mainContainer: {
+    backgroundColor: "#e8e8e8",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  topContainer: {
+    flex:1,
+    width: '100%',
+    backgroundColor: '#432344',
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24,
+    flexDirection: 'row',
+    padding: 30,
+  },
+
+  headerContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    justifyContent: "space-evenly",
+    
+  },
+
+  scrollContainer: {
+    flex:1,
+    flexGrow:5,
+    padding: 10,
+  },
+
+  bottomContainer: {
+    width:'100%',
+    height:50,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    justifyContent: 'center',
+  },
+
+
+  sidebarContainer: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: "flex-start",
+    alignItems:  "flex-end",
+    /*backgroundColor: "#000000" --> flex logic 1+1 = 2, 
+    so each flex takes 1/2 i.e half space in a row*/
+  },
+
+  iconContainer: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+
+  miniIconStyle: {
+    height: 30,
+    width: 30,
+    marginLeft: 20
+  },
+
+  button: {
+    borderRadius: 24,
+    height: 50,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-  
-    sidebarContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      alignItems: 'center',
-      /*backgroundColor: "#000000" --> flex logic 1+1 = 2, 
-      so each flex takes 1/2 i.e half space in a row*/
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  buttonText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 20,
+    color: '#000000',
+  },
+
+  imageStyle: {
+    width: 90,
+    height: 35,
+    alignSelf: 'center',
+    padding: 10,
+  },
+
+  tiles: {
+    margin: 15,
+    backgroundColor: '#ffffff',
+    borderRadius: 12,
+    height: 70,
+    width: 340,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-  
-    iconContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-    },
-  
-    miniIconStyle: {
-      height: 30,
-      width: 30,
-      marginLeft: 20,
-    },
-  
-    headerContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 20,
-      justifyContent: 'center',
-    },
-  
-    bottomContainer: {
-      height: '6%',
-      overflow: 'hidden',
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-      justifyContent: 'center',
-    },
-  
-    miniContainer: {
-      height: 70,
-      width: 70,
-      backgroundColor: '#FFC03D',
-      borderRadius: 12,
-      overflow: 'hidden',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  
-    button: {
-      borderRadius: 24,
-      height: 50,
-      width: 100,
-      alignSelf: 'center',
-      alignItems: 'center',
-      padding: 10,
-      marginRight: 10,
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-  
-    buttonText: {
-      fontFamily: 'Poppins-Bold',
-      fontSize: 20,
-      color: '#000000',
-    },
-  
-    imageStyle: {
-      width: 89,
-      height: 34,
-      alignSelf: 'center',
-      padding: 10,
-    },
-  
-    tiles: {
-      margin: 15,
-      backgroundColor: '#ffffff',
-      borderRadius: 12,
-      height: 70,
-      width: 340,
-      alignSelf: 'center',
-      alignItems: 'flex-start',
-      flexDirection: 'row',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-  
-    tilesText: {
-      fontFamily: 'Poppins-SemiBold',
-      fontSize: 28,
-      color: '#000000',
-      marginLeft: 15,
-      marginTop: 15,
-    },
-  });  
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  miniContainer: {
+    height: 70,
+    width: 70,
+    backgroundColor: '#FFC03D',
+    borderRadius: 12,
+    overflow: 'hidden',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  tilesText: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 28,
+    color: '#000000',
+    marginLeft: 15,
+    alignSelf: "center"
+  },
+});  
